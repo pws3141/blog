33baade7d34921ac3675a41927bcd54e22b364e0
pws3141 <psmith@posteo.net>
2024-12-20 12:11:37 +0000

    Built site for gh-pages

--- posts/01-highcharter/test.r
+++
@@ -1,6 +0,0 @@
-library(highcharter)
-
-hchart(penguins,"scatter", 
-       hcaes(x = flipper_length_mm, y = bill_length_mm, group = species)) |>
-  hc_add_dependency(name = "modules/accessibility.js") |> 
-
